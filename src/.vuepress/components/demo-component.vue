ba7f8b084d367166a3aa0316a5b8bb51b2a9546a
kushagrajohari <skjkushagra1309@gmail.com>
2022-04-29 23:20:24 +0530

    initial commit

---
+++ src/.vuepress/components/demo-component.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <buttonBasic text="Basic" class="btn-basic" />
+    <buttonDanger text="Danger" class="btn-danger" />
+    <buttonPrimary text="Primary" class="btn-primary" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "<button-basic >",
+    };
+  },
+};
+</script>
+
+<style>
+.btn-danger {
+  padding: 12px;
+  border: 1px solid white;
+  border-radius: 12px;
+  background-color: red !important;
+  color: white;
+}
+.btn-basic {
+  padding: 12px;
+  border: 1px solid white;
+  border-radius: 12px;
+  background-color: gray !important;
+  color: white;
+}
+.btn-primary {
+  padding: 12px;
+  border: 1px solid white;
+  border-radius: 12px;
+  background-color: skyblue !important;
+  color: white;
+}
+</style>
---
+++ src/guide/components.md
@@ -0,0 +1,14 @@
+# Components
+
+## Buttons
+
+Buttons have various types - basic, primary, danger. You can use the component as shown:
+
+```vue
+<!-- text = "text displayed on button"  -->
+<buttonBasic text="Basic" />
+<buttonDanger text="Danger" />
+<buttonPrimary text="Primary" />
+```
+
+<demo-component/>
